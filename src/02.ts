2d7f0515070bfead77eacd03710cb3cae05ca8e1
Judith Hurzlmeier <hurzlmei@fzi.de>
2023-12-10 17:41:00 +0100

    Day 2 part 2

--- src/02.ts
+++ src/02.ts
@@ -31,3 +31,38 @@ function partOne() {
     const result = (lines.length * (lines.length + 1)) / 2 - Array.from(impossibleGameNumbers).reduce((a, b) => a + b, 0)
     return result
 }
+
+class CubeMatrix  {
+    red: number = 0;
+    green: number = 0;
+    blue: number = 0
+}
+
+function partTwo(lines: string[]) {
+    const matrixCollection = new Array();
+
+    for (const line of lines) {
+        const result = [...line.matchAll(setsRegex)]
+        const matrix = new CubeMatrix();
+        const sets = result.map(elem => elem.groups.sets)
+
+        for (const set of sets) {
+            const colorCombinations = [...set.matchAll(numberColorRegex)].map(elem => elem.groups);
+            colorCombinations.forEach(combination => {
+                const currentValue = matrix[combination.color]
+                if (currentValue < combination.number) {
+                    matrix[combination.color] = Number(combination.number)
+                }
+            })
+        }
+        // console.log(matrix)
+        matrixCollection.push(matrix.blue * matrix.green * matrix.red)
+    }
+    console.log(matrixCollection)
+    const result = matrixCollection.reduce((a,b) => a+ b)
+    return result
+}
+
+const input: string[] = getLinesOfFileSplitBy('02');
+
+console.log(partTwo(input))
